4cdfd5ea3fbfcc3aa25821f26659495fc0ad38c8
cruiz311 <74808642+cruiz311@users.noreply.github.com>
2024-05-16 13:39:36 -0500

    Initial commit

---
+++ Assets/Scripts/AllienProblem/ChangeColor.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    public Renderer rend;
+    public MaterialPropertyBlock propblock;
+    public float intervaloTiempo = 0.5f;
+    private float tiempoPasado = 0f;
+
+    private void Awake()
+    {
+        rend = GetComponent<Renderer>();
+        propblock = new MaterialPropertyBlock();
+    }
+    void Start()
+    {
+    }
+
+    public void Update()
+    {
+        rend.GetPropertyBlock(propblock);
+        propblock.SetColor(name = "_Color", value: ChageColorAzar());
+        rend.SetPropertyBlock(propblock);
+
+        tiempoPasado += Time.deltaTime;
+        if (tiempoPasado >= intervaloTiempo)
+        {
+            
+        }
+    }
+
+    private Color ChageColorAzar()
+    {
+        return new Color(
+            r: Random.Range(0f, 1f),
+            g: Random.Range(0f, 1f),
+            b: Random.Range(0f, 1f));
+        
+    }
+}
